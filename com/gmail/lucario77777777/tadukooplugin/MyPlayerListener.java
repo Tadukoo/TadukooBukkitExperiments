12ecd05dccd0f6737f68621f487561a924c7ce39
Tadukoo <lucario77777777@gmail.com>
2013-07-05 02:20:07 -0400

    Created player listener java class

---
+++ com/gmail/lucario77777777/tadukooplugin/MyPlayerListener.java
@@ -0,0 +1,32 @@
+package com.gmail.lucario77777777.tadukooplugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
+
+@SuppressWarnings("deprecation")
+public class MyPlayerListener implements Listener {
+  public static tadukoopluginmain plugin;
+	public MyPlayerListener(tadukoopluginmain instance){
+		plugin = instance;
+	}
+	
+	@EventHandler
+	public void onPlayerChat(PlayerChatEvent event){
+		Player player = event.getPlayer();
+		
+		if(event.getMessage().toLowerCase().contains("heal")){
+			player.setHealth(20);
+			player.setFoodLevel(20);
+			player.sendMessage(ChatColor.GREEN + "Healed!");
+		}else if(event.getMessage().toLowerCase().contains("kill")){
+			player.setHealth(0);
+			player.sendMessage(ChatColor.DARK_RED + "You died!");
+		}else if(event.getMessage().toLowerCase().contains("sendme")){
+			player.sendMessage("Send You!");
+		}
+	}
+	
+}
